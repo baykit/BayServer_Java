0bd8a0f8de6159a76dbfbd8f34afe80f5b56d65b
Michisuke-P <michisukep@gmail.com>
2023-11-07 17:35:17 +0900

    [core] Resolves startup issue in daemon mode

--- test/simple/bin/bayserver.sh
+++ test/simple/bin/bayserver.sh
@@ -13,7 +13,7 @@ jar=`ls ${libdir} | grep "bayserver-[0-9].*.jar"`
  
 
 if [ "$daemon" = 1 ]; then
-   java $JAVA_OPTS -classpath ${jar} yokohama.baykit.bayserver.boot.Boot $* < /dev/null  > /dev/null 2>&1 &
+   java $JAVA_OPTS -classpath ${libdir}/${jar} yokohama.baykit.bayserver.boot.Boot $* < /dev/null  > /dev/null 2>&1 &
 else
    java $JAVA_OPTS -classpath ${libdir}/${jar} yokohama.baykit.bayserver.boot.Boot $* 
 fi
