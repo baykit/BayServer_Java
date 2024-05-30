92e9236b053ce2aaff3d69ac50e0af04bea36730
Michisuke-P <michisukep@gmail.com>
2024-05-30 19:59:52 +0900

    [core] Fixes some issues

--- modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/common/ReadOnlyShip.java
+++ modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/common/ReadOnlyShip.java
@@ -3,18 +3,12 @@ package yokohama.baykit.bayserver.common;
 import yokohama.baykit.bayserver.BayLog;
 import yokohama.baykit.bayserver.Sink;
 import yokohama.baykit.bayserver.agent.NextSocketAction;
-import yokohama.baykit.bayserver.agent.multiplexer.Transporter;
 import yokohama.baykit.bayserver.protocol.ProtocolException;
-import yokohama.baykit.bayserver.rudder.Rudder;
 import yokohama.baykit.bayserver.ship.Ship;
 
 import java.io.IOException;
 
 public abstract class ReadOnlyShip extends Ship {
-
-    protected void init(int agentId, Rudder rd, Transporter tp) {
-        super.init(agentId, rd, tp);
-    }
 
     /////////////////////////////////////
     // Implements Reusable
