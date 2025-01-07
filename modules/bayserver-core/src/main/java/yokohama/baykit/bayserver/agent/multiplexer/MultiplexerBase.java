b6361e3e923dcf638a3e475f533bf3730179ec8e
Michisuke-P <michisukep@gmail.com>
2025-01-07 11:28:19 +0900

    [core] Removes needless log.

--- modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/agent/multiplexer/MultiplexerBase.java
+++ modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/agent/multiplexer/MultiplexerBase.java
@@ -101,8 +101,6 @@ public abstract class MultiplexerBase implements Multiplexer {
         catch(IOException e) {
             BayLog.error(e);
         }
-
-        BayLog.trace("%s Flush buffer", agent);
     }
 
     @Override
