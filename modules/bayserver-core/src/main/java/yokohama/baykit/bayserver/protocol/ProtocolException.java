7206dcea548a443369e55d939aab1281317ece55
Michisuke-P <michisukep@gmail.com>
2025-12-22 10:05:54 +0900

    [core] Adds comment on ProtocolException
     shutdown when the path contains invalid characters

--- modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/protocol/ProtocolException.java
+++ modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/protocol/ProtocolException.java
@@ -2,6 +2,12 @@ package yokohama.baykit.bayserver.protocol;
 
 import java.io.IOException;
 
+/**
+ * ProtocolException is thrown when protocol-level violations are detected,
+ * such as invalid packet framing or incorrect packet ordering.
+ * (Invalid HTTP headers or content length values result in an HttpException,
+ * which causes a 400 Bad Request response to be returned to the client.)
+ */
 public class ProtocolException extends IOException {
     public ProtocolException(String message) {
         super(message);
