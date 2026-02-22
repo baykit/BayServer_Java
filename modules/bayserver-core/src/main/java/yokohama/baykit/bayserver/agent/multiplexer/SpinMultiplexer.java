60c78e83e9310c9dce055b71e4014ed4a7af4171
Michisuke-P <michisukep@gmail.com>
2026-02-22 07:16:11 +0000

    [core] Move WriteUnit class for refactoring

--- modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/agent/multiplexer/SpinMultiplexer.java
+++ modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/agent/multiplexer/SpinMultiplexer.java
@@ -5,6 +5,7 @@ import yokohama.baykit.bayserver.Sink;
 import yokohama.baykit.bayserver.agent.GrandAgent;
 import yokohama.baykit.bayserver.agent.TimerHandler;
 import yokohama.baykit.bayserver.common.RudderState;
+import yokohama.baykit.bayserver.common.WriteUnit;
 import yokohama.baykit.bayserver.rudder.AsynchronousFileChannelRudder;
 import yokohama.baykit.bayserver.rudder.ChannelRudder;
 import yokohama.baykit.bayserver.rudder.InputStreamRudder;
