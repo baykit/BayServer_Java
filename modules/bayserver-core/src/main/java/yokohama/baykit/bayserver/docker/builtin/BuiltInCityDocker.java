e77609aa8c3a952764914b4ffb27afe873b6c5e5
Michisuke-P <michisukep@gmail.com>
2026-04-02 19:10:04 +0000

    [core] Fix missing req content consume in onReadReqContent implementations

--- modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/docker/builtin/BuiltInCityDocker.java
+++ modules/bayserver-core/src/main/java/yokohama/baykit/bayserver/docker/builtin/BuiltInCityDocker.java
@@ -208,7 +208,7 @@ public class BuiltInCityDocker extends DockerBase implements City {
                     tur.req.setReqContentHandler(new ReqContentHandler() {
                         @Override
                         public void onReadReqContent(Tour tur, byte[] buf, int start, int len, ContentConsumeListener lis) throws IOException {
-
+                            tur.req.consumed(Tour.TOUR_ID_NOCHECK, len, lis);
                         }
 
                         @Override
